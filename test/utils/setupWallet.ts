12c9fb81e59b0d11ee2fd84e3343c7c9bb3f0953
Gulam Rasul Shah <99994920+grsLammy@users.noreply.github.com>
2022-10-31 15:21:04 +0530

    feat: refactor code

--- test/utils/setupWallet.ts
+++ test/utils/setupWallet.ts
@@ -1,20 +1,32 @@
 import dotenv from "dotenv";
 dotenv.config();
-import { ethers } from "ethers";
+import { ethers, providers, Signer } from "ethers";
 
 const mnemonics: any = process.env.MNEMONICS;
+const zkEVM_RPC: any = process.env.ZKEVM_RPC_URL;
+
+let zkEVM_provider: any;
+let ownerSigner: Signer, adminSigner: Signer, userSigner: Signer, aliceSigner: Signer, bobSigner: Signer;
 
 const setupWallets = async () => {
     try {
         let derivedNodeArray: any = [];
+        // get zkEVM provider
+        zkEVM_provider = new providers.JsonRpcProvider(zkEVM_RPC);
         const HDNode = ethers.utils.HDNode.fromMnemonic(mnemonics);
         for (let i = 0; i < 5; i++) {
             derivedNodeArray[i] = HDNode.derivePath(`m/44'/60'/0'/0/${i}`);
         }
+        ownerSigner = new ethers.Wallet(derivedNodeArray[0].privateKey, zkEVM_provider);
+        adminSigner = new ethers.Wallet(derivedNodeArray[1].privateKey, zkEVM_provider);
+        userSigner = new ethers.Wallet(derivedNodeArray[2].privateKey, zkEVM_provider);
+        aliceSigner = new ethers.Wallet(derivedNodeArray[3].privateKey, zkEVM_provider);
+        bobSigner = new ethers.Wallet(derivedNodeArray[4].privateKey, zkEVM_provider);
+
         return derivedNodeArray;
     } catch (error) {
         console.log("error while setting up wallets: ", error);
     }
 };
 
-export { setupWallets };
+export { setupWallets, zkEVM_provider, ownerSigner, adminSigner, userSigner, aliceSigner, bobSigner };

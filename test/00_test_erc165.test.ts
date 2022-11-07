49ba8896658d7abc846cc25518d006451a294a38
vakul <abhisheksaxena714.vs@gmail.com>
2022-11-07 11:51:05 +0530

    second commit

--- test/00_test_erc165.test.ts
+++ test/00_test_erc165.test.ts
@@ -1,46 +1,40 @@
 import { expect } from "chai";
 import { ethers, Contract } from "ethers";
+import { abi, bytecode } from '../artifacts/src/zkevm_erc165.sol/TestERC165.json';
+import { setupWallets, zkEVM_provider, ownerSigner, userSigner, aliceSigner } from "./utils/setupWallet";
+import dotenv from "dotenv";
+import { checkBalances } from "./utils/checkBalances";
+dotenv.config();
 
-describe("ERC165",function(){
+describe("ERC165",async function(){
     
     let TestERC165:any;
-    const provider:any = new ethers.providers.JsonRpcProvider('https://public.zkevm-test.net:2083/');
+    
+    const derivedNode= await setupWallets();
+    before(async () => {
 
-    it("Interface_ID Availability",async function(){
+
+        const TestERC165factory = new ethers.ContractFactory(abi, bytecode,ownerSigner);
+    
+        console.log("Checking if wallet addresses have any balance....");
+        await checkBalances(derivedNode);
+        console.log("\nDeploying ERC165TEST smart contract on zkEVM chain....");
+        const testERC165 = await TestERC165factory.deploy();
+        await testERC165.deployed();
+
+        TestERC165 = new Contract(testERC165.address, abi,zkEVM_provider);
         
-        // const [owner] = await ethers.getSigners();
-        // console.log("Signers object", owner);
-        // const TestERC165 =await ethers.getContractFactory("TestERC165");
-
-        // const testERC165 = TestERC165.deploy();
-
-        // const result = await testERC165.supportsInterface(0x01ffc9a7);
-
-        // console.log(result);
-
-        TestERC165 = new Contract('0xA0a1d9f4149aa1D7F79Dcf085B863C30Fec3C0De',[
-            {
-                "inputs": [
-                    {
-                        "internalType": "bytes4",
-                        "name": "interfaceId",
-                        "type": "bytes4"
-                    }
-                ],
-                "name": "supportsInterface",
-                "outputs": [
-                    {
-                        "internalType": "bool",
-                        "name": "",
-                        "type": "bool"
-                    }
-                ],
-                "stateMutability": "view",
-                "type": "function"
-            }
-        ],provider);
-
-        expect(await TestERC165.supportsInterface(0x01ffc9a7)).to.equal(true);
+        console.log("\nERC165Test contract deployed at: ", testERC165.address);
+        console.log(
+            `Contract Details: https://explorer.public.zkevm-test.net/address/${testERC165.address}`
+        );
+        console.log("\n");
+    });
+    describe("ERC165 tests", async () => {
+        it('Interface_ID Availability', async () => {
+            expect(await TestERC165.supportsInterface(0x01ffc9a7)).to.equal(true);    
+        });
+        expect(await TestERC165.supportsInterface(0x01ffc9a7)).to.equal(true);        
 
         
     })

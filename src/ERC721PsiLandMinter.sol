1c8299ad0ed55e5412d5ea61be9a08bf2ed3f24e
Gulam Rasul Shah <99994920+grsLammy@users.noreply.github.com>
2022-11-16 13:26:11 +0530

    feat: match solidity version

--- src/ERC721PsiLandMinter.sol
+++ src/ERC721PsiLandMinter.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity 0.8.7;
 import "./utils/AccessProtected.sol";
 import "erc721psi/contracts/ERC721Psi.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";

d8d14e2d71dbb055f1dbacefb9399641f022e687
Gulam Rasul Shah <99994920+grsLammy@users.noreply.github.com>
2022-11-16 13:43:48 +0530

    feat: OZ standard ERC token deployment scripts

---
+++ scripts/ERC1155/deploy_ERC1155Token.ts
@@ -0,0 +1,18 @@
+import { ethers } from "hardhat";
+import { TestTokenERC1155__factory } from "../../src/types";
+
+async function deploy() {
+    // get the contract to deploy
+    const ERC1155 = (await ethers.getContractFactory("TestTokenERC1155")) as TestTokenERC1155__factory;
+    const erc1155 = await ERC1155.deploy();
+    console.log("\nDeploying ERC1155 Token smart contract on zkEVM chain....");
+    function delay(ms: number) {
+        return new Promise((resolve) => setTimeout(resolve, ms));
+    }
+
+    await delay(20000);
+    console.log("\nERC1155 Token contract deployed at: ", erc1155.address);
+    console.log(`Contract Details: https://explorer.public.zkevm-test.net/address/${erc1155.address}`);
+}
+
+deploy();
---
+++ scripts/ERC20/deploy_ERC20Token.ts
@@ -0,0 +1,18 @@
+import { ethers } from "hardhat";
+import { TestTokenERC20__factory } from "../../src/types";
+
+async function deploy() {
+    // get the contract to deploy
+    const ERC20 = (await ethers.getContractFactory("TestTokenERC20")) as TestTokenERC20__factory;
+    const erc20 = await ERC20.deploy();
+    console.log("\nDeploying ERC20 Token smart contract on zkEVM chain....");
+    function delay(ms: number) {
+        return new Promise((resolve) => setTimeout(resolve, ms));
+    }
+
+    await delay(20000);
+    console.log("\nERC20 Token contract deployed at: ", erc20.address);
+    console.log(`Contract Details: https://explorer.public.zkevm-test.net/address/${erc20.address}`);
+}
+
+deploy();

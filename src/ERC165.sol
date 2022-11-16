ec27d9d0158a49e5075da7fc421470f69c7104ef
Gulam Rasul Shah <99994920+grsLammy@users.noreply.github.com>
2022-11-16 13:25:52 +0530

    feat: match soldity version and proper indentation

--- src/ERC165.sol
+++ src/ERC165.sol
@@ -1,13 +1,10 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.2;
+pragma solidity 0.8.7;
 
 import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 
-contract TestERC165 is ERC165{
-
-
-function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
-    return interfaceId == type(IERC165).interfaceId || super.supportsInterface(interfaceId);
-}
-
+contract TestERC165 is ERC165 {
+    function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
+        return interfaceId == type(IERC165).interfaceId || super.supportsInterface(interfaceId);
+    }
 }

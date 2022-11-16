9bf878860401fa4358f0a0516106485d78c96e46
Gulam Rasul Shah <99994920+grsLammy@users.noreply.github.com>
2022-11-16 13:14:50 +0530

    feat: update prefix

--- test/10_Fallback.test.ts
+++ test/10_Fallback.test.ts
@@ -1,13 +1,12 @@
 import { expect } from "chai";
 import dotenv from "dotenv";
 dotenv.config();
-import { setupWallet, zkEVM_provider, ownerSigner, userSigner, aliceSigner } from "./utils/setupWallet";
+import { setupWallet, zkEVM_provider, ownerSigner } from "./utils/setupWallet";
 import { ethers, Contract } from "ethers";
 import { checkBalances } from "./utils/checkBalances";
 import fallback_artifacts from "../artifacts/src/fallback.sol/Fallback.json";
 import sendToFallback_artifacts from "../artifacts/src/fallback.sol/SendToFallback.json";
-import { send } from "process";
-const sleep = ms => new Promise(res => setTimeout(res, ms));
+const sleep = (ms) => new Promise((res) => setTimeout(res, ms));
 
 describe("Fallback contract deployment & tests on zkEVM", async () => {
     // declare an instance of the contract to be deployed
@@ -17,11 +16,19 @@ describe("Fallback contract deployment & tests on zkEVM", async () => {
     // setup atleast 5 wallet addresses for testing
     const derivedNode = await setupWallet();
     before(async () => {
-        console.log("\nFallback UNIT TEST CASES\n");
+        console.log("\nFALLBACK UNIT TEST CASES\n");
 
         // get the contract factory
-        const fallback_factory = new ethers.ContractFactory(fallback_artifacts.abi, fallback_artifacts.bytecode, ownerSigner);
-        const sendToFallback_factory = new ethers.ContractFactory(sendToFallback_artifacts.abi, sendToFallback_artifacts.bytecode, ownerSigner);
+        const fallback_factory = new ethers.ContractFactory(
+            fallback_artifacts.abi,
+            fallback_artifacts.bytecode,
+            ownerSigner
+        );
+        const sendToFallback_factory = new ethers.ContractFactory(
+            sendToFallback_artifacts.abi,
+            sendToFallback_artifacts.bytecode,
+            ownerSigner
+        );
 
         console.log("Checking if wallet addresses have any balance....");
         await checkBalances(derivedNode);
@@ -34,12 +41,15 @@ describe("Fallback contract deployment & tests on zkEVM", async () => {
 
         // wait for the contract to get deployed
         await fallback_contract.deployed();
-        await sleep(5000); //5 second delay
         await sendToFallback_contract.deployed();
 
         // get the instance of the deployed contract
         fallback = new Contract(fallback_contract.address, fallback_artifacts.abi, zkEVM_provider);
-        sendToFallback = new Contract(sendToFallback_contract.address, sendToFallback_artifacts.abi, zkEVM_provider);
+        sendToFallback = new Contract(
+            sendToFallback_contract.address,
+            sendToFallback_artifacts.abi,
+            zkEVM_provider
+        );
 
         console.log("\nFallback contract deployed at: ", sendToFallback.address);
         console.log(
@@ -49,24 +59,20 @@ describe("Fallback contract deployment & tests on zkEVM", async () => {
     });
 
     describe("Fallback contract functionalities tests", async () => {
-
-        it("can transfer to fallback", async () => {
-
+        it("...can transfer to fallback", async () => {
             const tx = await sendToFallback
                 .connect(ownerSigner)
                 .transferToFallback(fallback.address, { value: ethers.utils.parseEther("0.00001") });
-            await tx.wait()
+            await tx.wait(1);
             expect(await fallback.getBalance()).eq(ethers.utils.parseEther("0.00001"));
         });
 
-        it("can call fallback", async () => {
-
+        it("...can call fallback", async () => {
             const tx = await sendToFallback
                 .connect(ownerSigner)
                 .callFallback(fallback.address, { value: ethers.utils.parseEther("0.00001") });
-            await tx.wait()
+            await tx.wait(1);
             expect(await fallback.getBalance()).eq(ethers.utils.parseEther("0.00002"));
         });
-
     });
 });

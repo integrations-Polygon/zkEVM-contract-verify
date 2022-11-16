1c4a6d2d4d7b393ce5ec1a7e414448aa96ff98ed
Gulam Rasul Shah <99994920+grsLammy@users.noreply.github.com>
2022-11-16 13:27:56 +0530

    feat: only import necessary signers

--- test/09_interface.test.ts
+++ test/09_interface.test.ts
@@ -1,7 +1,7 @@
 import { expect } from "chai";
 import dotenv from "dotenv";
 dotenv.config();
-import { setupWallet, zkEVM_provider, ownerSigner, userSigner, aliceSigner } from "./utils/setupWallet";
+import { setupWallet, zkEVM_provider, ownerSigner } from "./utils/setupWallet";
 import { ethers, Contract } from "ethers";
 import { checkBalances } from "./utils/checkBalances";
 import counter_artifacts from "../artifacts/src/interface.sol/Counter.json";
